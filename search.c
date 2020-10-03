fd9a065af4abe61b16f5b4a32a52578d6bd6e12f
Daniele Castiglia <jdanya@outlook.com>
2020-10-03 21:50:05 +0200

    Added search.c, that will use the threads to search throw the file

---
+++ search.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "utilities.h"
+
+void search(FILE **__f, const char *__passwd)
+{
+    
+}
--- utilities.h
+++ utilities.h
@@ -8,4 +8,6 @@
 pthread_t threads[2]; // 2 Thread per cercare nel file
 pthread_mutex_t lock; // Mutex
 
+extern void search(FILE **__f, const char *__passwd);
+
 #endif // UTILITIES_H
