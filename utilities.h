8f0bd17f6f1f59c4d739721ce5c5f1ab4e8e4ead
Daniele Castiglia <jdanya@outlook.com>
2020-10-03 21:47:46 +0200

    Start defining var. mutex and array threads

--- utilities.h
+++ utilities.h
@@ -1,6 +1,11 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
+#include <unistd.h>
+#include <pthread.h>
 
 #define MAX_BUF 256
+
+pthread_t threads[2]; // 2 Thread per cercare nel file
+pthread_mutex_t lock; // Mutex
 
 #endif // UTILITIES_H
